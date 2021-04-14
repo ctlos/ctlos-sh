5909b8ffe56dfb01e50a9f87fa2cab45b166801c
creio <mailcreio@gmail.com>
2021-04-14 09:50:28 +0300

    14.04.2021 Update

--- settings.sh
+++ settings.sh
@@ -10,4 +10,11 @@ rm strap.sh
 
 mkdir /media
 
+cat <<LOL >>/etc/pacman.d/gnupg/gpg.conf
+keyserver hkp://keys.gnupg.net
+keyserver hkp://pool.sks-keyservers.net:80
+keyserver hkps://hkps.pool.sks-keyservers.net:443
+keyserver hkp://ipv4.pool.sks-keyservers.net:11371
+LOL
+
 rm /usr/local/bin/settings.sh
