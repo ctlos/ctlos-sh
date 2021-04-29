6f02a943f53b1b1b41fdf7d68ffa376c52d01458
creio <mailcreio@gmail.com>
2021-04-29 11:44:41 +0300

    29.04.2021 Update

--- settings.sh
+++ settings.sh
@@ -11,14 +11,10 @@ rm strap.sh
 mkdir /media
 
 cat <<LOL >>/etc/pacman.d/gnupg/gpg.conf
-keyserver hkp://keys.openpgp.org
-keyserver hkp://pgp.mit.edu
-keyserver hkp://keyring.debian.org
-keyserver hkp://keyserver.ubuntu.com
-keyserver hkp://keys.gnupg.net
-keyserver hkp://pool.sks-keyservers.net:80
 keyserver hkps://hkps.pool.sks-keyservers.net:443
 keyserver hkp://ipv4.pool.sks-keyservers.net:11371
+keyserver hkp://pgp.mit.edu
+keyserver hkp://keys.gnupg.net
 LOL
 
 rm /usr/local/bin/settings.sh
