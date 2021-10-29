a857ab9f73d1adaefa22456995f9c94629c26b36
creio <mailcreio@gmail.com>
2021-10-29 11:53:25 +0300

    29.10.2021 Update

--- postinstall.sh
+++ postinstall.sh
@@ -3,6 +3,14 @@
 ## calamares shellprocess_postinstall.conf
 # run chroot
 
+clean_post() {
+  local files_rm=(
+    /etc/systemd/system/multi-user.target.wants/{choose-mirror.service,pacman-init.service,reflector.service}
+  )
+  local i
+  for i in ${files_rm[*]}; do rm -rf $i; done
+}
+
 enable_dm() {
   rm /etc/systemd/system/display-manager.service
   [[ $(pacman -Qs sddm) ]] && systemctl enable sddm
@@ -11,6 +19,8 @@ enable_dm() {
   [[ $(pacman -Qs gdm) ]] && systemctl enable gdm
   # systemctl set-default graphical.target
 }
+
+clean_post
 enable_dm
 
 rm /usr/local/bin/postinstall.sh
