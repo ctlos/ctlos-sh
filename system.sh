9d4ce0c01830a4e2ce22e3340b27725c93b63c0b
creio <mailcreio@gmail.com>
2024-04-02 23:19:32 +0300

    02.04.2024 Update

--- system.sh
+++ system.sh
@@ -32,10 +32,10 @@ cp -rf /etc/motd ${chroot_path}/etc
 cp -rf /etc/ntp.conf ${chroot_path}/etc
 cp -rf /etc/sddm.conf.d ${chroot_path}/etc
 
-# if [ -f "${chroot_path}/etc/sddm.conf" ]; then
-#     cat ${chroot_path}/etc/sddm.conf
-# else
-#     touch ${chroot_path}/etc/sddm.conf
+if [ -f "${chroot_path}/etc/sddm.conf" ]; then
+    cat ${chroot_path}/etc/sddm.conf
+else
+    touch ${chroot_path}/etc/sddm.conf
 #     cat <<LOL >${chroot_path}/etc/sddm.conf
 # [Autologin]
 # Relogin=false
@@ -43,7 +43,7 @@ cp -rf /etc/sddm.conf.d ${chroot_path}/etc
 # Session=
 # User=
 # LOL
-# fi
+fi
 
 cp -rf /etc/pamac.conf ${chroot_path}/etc
 cp -rf /etc/pacman.conf ${chroot_path}/etc
