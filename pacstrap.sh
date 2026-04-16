29993a312719d22da5248ad531588117cfabd73b
creio <mailcreio@gmail.com>
2026-04-16 21:51:02 +0300

    16.04.2026 Update

--- pacstrap.sh
+++ pacstrap.sh
@@ -8,7 +8,7 @@ chroot_path=$(cat /tmp/chroot_path.tmp)
 exec > >(tee /var/log/ctlos.log) 2>&1
 
 pacman -S reflector --noconfirm --needed
-reflector -l 7 -p https,http --sort rate --save /etc/pacman.d/mirrorlist
+reflector -a 12 -l 10 -p https,http --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy archlinux-keyring --noconfirm
 
 if [[ $(command -v pacstrap) ]]; then
@@ -64,7 +64,7 @@ chmod +x ${chroot_path}/usr/local/bin/postinstall.sh
 
 echo "=== add repo archlinuxcn ==="
 pacman -U --noconfirm https://repo.archlinuxcn.org/x86_64/archlinuxcn-keyring-20250506-1-any.pkg.tar.zst
-echo -e '\n[archlinuxcn]\nServer = https://repo.archlinuxcn.org/$arch\n' | tee -a /etc/pacman.conf >/dev/null
+echo -e '\n\n[archlinuxcn]\nServer = https://repo.archlinuxcn.org/$arch\n' | tee -a /etc/pacman.conf >/dev/null
 tail -n 5 /etc/pacman.conf
 pacman -Syy
 
