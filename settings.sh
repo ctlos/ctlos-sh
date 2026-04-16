a7f1f60962d47c9567ad84b3deab0b10c58f0600
creio <mailcreio@gmail.com>
2026-04-17 00:41:22 +0300

    17.04.2026 Update

--- settings.sh
+++ settings.sh
@@ -3,19 +3,22 @@
 ## calamares shellprocess_settings.conf
 # run chroot
 
+echo "[+] Updating archlinuxcn-keyring..."
+pacman -Sy --noconfirm archlinuxcn-keyring || {
+    echo "[-] Failed to update keyring, trying full refresh..."
+    pacman -Syyu --noconfirm archlinuxcn-keyring
+}
+
 mkdir /media
 
 haveged -w 1024
 pacman-key --init
-pkill haveged
 pacman-key --populate
+pkill haveged
 
 cat <<LOL >>/etc/pacman.d/gnupg/gpg.conf
-keyserver hkps://hkps.pool.sks-keyservers.net:443
-keyserver hkp://ipv4.pool.sks-keyservers.net:11371
-keyserver hkp://pgp.mit.edu
-keyserver hkp://keys.gnupg.net
-keyserver hkp://keyserver.ubuntu.com
+keyserver = keys.openpgp.org
+keyserver = keyserver.ubuntu.com
 LOL
 
 rm /usr/local/bin/settings.sh
